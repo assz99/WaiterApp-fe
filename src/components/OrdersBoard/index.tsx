d4f5d22caf4f34fc7bbf123418ad98a5d4c17a79
Assz <renanguides@hotmail.com>
2023-03-10 02:07:38 -0400

    Montado o Modal quando se clica no orderBoard

--- src/components/OrdersBoard/index.tsx
+++ src/components/OrdersBoard/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Order } from '../../types/Orders'
+import OrderModal from '../OrderModal'
 import { Board, OrdersContainer } from './styles'
 
 interface OrdersBoardProps {
@@ -8,28 +9,48 @@ interface OrdersBoardProps {
   orders: Order[]
 }
 
-export default function OrdersBoard ({ icon, title }: OrdersBoardProps) {
+export default function OrdersBoard ({ icon, title, orders }: OrdersBoardProps) {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [selectedOrder, setSelectedOrder] = useState<null | Order>(null)
+
+  function handleOpenModal (order: Order) {
+    setIsModalVisible(true)
+    setSelectedOrder(order)
+  }
+
+  function handleCloseModal () {
+    setIsModalVisible(false)
+    setSelectedOrder(null)
+  }
+
   return (
     <Board>
+      <OrderModal
+        visible={isModalVisible}
+        order={selectedOrder}
+        onClose={handleCloseModal}
+      />
+
       <header>
         <span>{icon}</span>
         <strong>{title}</strong>
-        <span>(1)</span>
+        <span>{orders.length}</span>
       </header>
 
-      <OrdersContainer>
-        <button type='button'>
-          <strong>Mesa 1</strong>
-          <span>2 itens</span>
-        </button>
-      </OrdersContainer>
-
-      <OrdersContainer>
-        <button type='button'>
-          <strong>Mesa 2</strong>
-          <span>2 itens</span>
-        </button>
-      </OrdersContainer>
+      {orders.length > 0 && (
+        <OrdersContainer>
+          {orders.map(order => (
+            <button
+              type='button'
+              key={order._id}
+              onClick={() => handleOpenModal(order)}
+            >
+              <strong>Mesa {order.table}</strong>
+              <span>{order.products.length} itens</span>
+            </button>
+          ))}
+        </OrdersContainer>
+      )}
     </Board>
   )
 }
